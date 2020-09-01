3fa4acb696b3da0190a7305e0cd233f130bd4220
Tonny1494 <tonny_espinoza1494@hotmail.com>
2020-09-01 16:20:48 -0500

    se cambio la urls

--- realidadoxxo/urls.py
+++ realidadoxxo/urls.py
@@ -36,3 +36,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
