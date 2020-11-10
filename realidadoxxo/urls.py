17cc442c7a00206769e2db270a544964bfb03988
unknown <tony_espinoza1494@hotmail.com>
2020-11-10 17:19:09 -0500

    Recuperar Contraseña

--- realidadoxxo/urls.py
+++ realidadoxxo/urls.py
@@ -61,7 +61,7 @@ urlpatterns = [
     
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name = "productos/password/password_reset_form.html"), name ='password_reset'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "productos/password/password_reset_done.html"), name ='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "productos/password/password_reset_confirms.html"), name ='password_reset_confirm'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "productos/password/password_reset_confirm.html"), name ='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "productos/password/password_reset_complete.html"), name ='password_reset_complete')
 ]
 
